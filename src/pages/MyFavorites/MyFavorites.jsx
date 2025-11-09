723019c18e14b6ddce52f5cd735e3ff6f35e02ff
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-10 05:16:57 +0600

    my favorites review displayed

--- src/pages/MyFavorites/MyFavorites.jsx
+++ src/pages/MyFavorites/MyFavorites.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure.jsx";
+import AuthContext from "../../contexts/AuthContext.jsx";
+import Loading from "../../components/Loading/Loading.jsx";
+import ReviewCard from "../../components/ReviewCard.jsx";
 
 const MyFavorites = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const axiosSecure = useAxiosSecure();
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        if (user) {
+            axiosSecure.get(`/favorites?reviewerEmail=${user.email}`)
+                .then(data => {
+                    setData(data.data);
+                    setLoading(false);
+                })
+        }
+    }, [user]);
+
+    if (loading) {
+        return <Loading />
+    }
+
     return (
         <div>
             <h1>My Favourites page</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+                {
+                    data.map(review => <ReviewCard key={review._id} review={review} />)
+                }
+            </div>
         </div>
     );
 };
