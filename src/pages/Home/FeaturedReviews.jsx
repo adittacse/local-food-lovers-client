72c27e574e0c2fd7b2fb78c1d03c1cf5933c3f9c
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-11 04:22:47 +0600

    design improved

--- src/pages/Home/FeaturedReviews.jsx
+++ src/pages/Home/FeaturedReviews.jsx
@@ -26,9 +26,7 @@ const FeaturedReviews = () => {
             <h2 className="text-2xl md:text-3xl font-bold text-center">Featured Reviews</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
                 {
-                    data.map(r => (
-                        <ReviewCard key={r._id} review={r} />
-                    ))
+                    data.map(review => <ReviewCard key={review._id} review={review} />)
                 }
             </div>
             <div className="flex justify-center mt-6">
