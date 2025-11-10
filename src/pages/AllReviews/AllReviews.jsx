6313ad2749b6554825174905d4401297190ac1e3
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-10 16:26:05 +0600

    live search implemented

--- src/pages/AllReviews/AllReviews.jsx
+++ src/pages/AllReviews/AllReviews.jsx
@@ -15,33 +15,40 @@ const AllReviews = () => {
                 setReviews(data.data);
                 setLoading(false);
             })
-    }, []);
+    }, [axios]);
 
     const handleSearch = () => {
         setLoading(true);
         const text = searchRef.current.value.trim().toLowerCase();
 
-        setTimeout(() => {
-            if (text === "") {
-                axios.get("/reviews")
-                    .then(data => {
-                        setReviews(data.data);
-                        setLoading(false);
-                    })
-            } else {
-                const result = reviews.filter(review => review.foodName.toLowerCase().includes(text));
-                setReviews(result);
-                setLoading(false);
-            }
-        }, 0);
+        if (text === "") {
+            axios.get("/reviews")
+                .then(data => {
+                    setReviews(data.data);
+                    setLoading(false);
+                })
+        } else {
+            // axios.get(`/reviews?foodName=${text}`)
+            axios.get("/reviews", {
+                params: {
+                    foodName: text
+                }
+            })
+                .then(data => {
+                    setReviews(data.data);
+                    setLoading(false);
+                })
+        }
     }
 
-    // if (reviews.length === 0) {
-    //     return <p className="mt-6 text-center text-gray-500">No reviews found.</p>;
-    // }
-
-    if (loading) {
-        return <Loading />;
+    const handleShowAllReviews = () => {
+        searchRef.current.value = "";
+        setLoading(true);
+        axios.get("/reviews")
+            .then(data => {
+                setReviews(data.data);
+                setLoading(false);
+            })
     }
 
     return (
@@ -49,20 +56,41 @@ const AllReviews = () => {
             <div>
                 <h2 className="text-2xl md:text-3xl font-bold text-center">All Reviews</h2>
                 <div className="flex justify-end gap-4 mt-6">
-                    <form className="join">
-                        <input ref={searchRef} onChange={handleSearch} type="search" className="input input-bordered join-item" placeholder="Search by food name" />
-                        <button className="btn btn-primary join-item" type="submit">
-                            Search
-                        </button>
-                    </form>
+                    <label className="input">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor">
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input ref={searchRef} onChange={handleSearch} type="search" placeholder="Search by food name" />
+                    </label>
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-                {
-                    reviews.map(review => <ReviewCard key={review._id} review={review} />)
-                }
-            </div>
+            {
+                loading ? (
+                        <div className="grid place-items-center min-h-36 mt-10">
+                            <Loading />
+                        </div>
+                    )
+                    : reviews.length === 0 ? (
+                        <div className="flex flex-col justify-center items-center min-h-36">
+                            <h3 className="font-bold text-4xl text-[#627382] mt-10 mb-6">No Reviews Found</h3>
+
+                            <button onClick={handleShowAllReviews} className="btn font-semibold text-white bg-[linear-gradient(90deg,#632EE3_0%,#9F62F2_100%)] rounded-sm px-[39px] py-[15px]">
+                                Show All Reviews
+                            </button>
+                        </div>
+                    )
+                    : (
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+                            {
+                                reviews.map(review => <ReviewCard key={review._id} review={review} />)
+                            }
+                        </div>
+                    )
+            }
         </section>
     );
 };
